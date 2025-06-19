77d53fd269e1d631beb3bc9c8b9e6725040f0ced
Chandanadara2025 <chandana.dara@scrumconnect.com>
2025-06-19 19:55:50 +0530

    Update AuthService.cs

--- backend/auth-service/Services/AuthService.cs
+++ backend/auth-service/Services/AuthService.cs
@@ -2,6 +2,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.Extensions.Configuration;
 
 namespace auth_service.Services
 {
